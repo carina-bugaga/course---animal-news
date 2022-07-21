2d7bbb7335c7aaec5b4b3a5ce56a4e96ca7b934d
Carina B <carina.bugaga@gmail.com>
2022-07-21 11:03:19 +0300

    добавлен курс валют

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 $(function () {
-    $('#btn-allNews')
-       /* .click(function () {
-            $('.news-comments-count').each(function () {
-                if ($(this).text() >= 20) {
-                    $(this).css('color', 'blue');
-                }
-            });
-        });*/
-
-    $('.news-time').click(function(){
+    $('.news-time').click(function () {
         $(this).parent().parent().css({
-            border : '0.3px solid #2196F3',
+            border: '0.3px solid #2196F3',
             'border-radius': '20px',
         });
     });
+    //EAOdbvUYDRw6ClOC2rOewoDBq7gbcUmm
+   
+});
+
+$(document).ready(function () {
+    $.get(
+        'https://www.cbr-xml-daily.ru/latest.js',
+        function (data) {
+            let response = JSON.parse(data);
+            let usd = (1/response.rates.USD).toFixed(2);
+            let eur = (1/response.rates.EUR).toFixed(2);
+            $('.dollar').append(usd);
+            $('.euro').append(eur);
+        });
 });
